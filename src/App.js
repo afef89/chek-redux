080b14f8eb380fdc9a9ff46608e5a45e1b1979ef
afef89 <mabrouk_afef@yahoo.fr>
2022-01-17 08:10:23 +0100

    my project redux here

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import "./styles.css";
+import InputField from "./components/InputField";
+import TodosList from "./components/TodosList";
+import { deleteAll, persistTodos } from "./redux/action/addTodo.action";
+import { connect } from "react-redux";
+const App = ({ deleteAll, persistTodos }) => {
+  useEffect(() => {
+    persistTodos();
+  }, [persistTodos]);
+  return (
+    <div className="App">
+      <h1 style={{ textDecoration: "underline" }}> Todos App</h1>
+      <InputField />
+      <TodosList />
+      
+    </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  deleteAll: () => dispatch(deleteAll()),
+  persistTodos: () => dispatch(persistTodos())
+});
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
